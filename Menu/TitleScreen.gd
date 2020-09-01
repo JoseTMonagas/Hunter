1a2c6315dea25388459721efe3d73a90195dd287
Ismail Amer <imav97@hotmail.com>
2020-09-01 10:54:44 -0400

    ADDed functionality to 'insert coin'

--- Menu/TitleScreen.gd
+++ Menu/TitleScreen.gd
@@ -15,3 +15,8 @@ func _on_NewGame_pressed():
 
 func _on_ExitGame_pressed():
 	get_tree().quit()
+
+
+func _on_InsertCoin_pressed():
+	get_tree().change_scene("res://Map/Map.tscn")
+	pass # Replace with function body.
